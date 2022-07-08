9fb5ee9fe930759cdb5fca402656e0a770d68549
vladibuyanov <vladibuyanov@gmail.com>
2022-07-08 16:32:13 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from telebot import TeleBot
+
+bot = TeleBot(os.environ.get('TOKEN'))
+ID = 5155228261
+# ID = {'V/B': 542276549, 'V/R': 1325734, 'A/B': 220305114]
+text = 'ДАННАЯ ХРЮКАНИНА СОЗДАНА И (ИЛИ) РАСПРОСТРАНЕНА ПОГАНЫМ ЖАЛОМ, ' \
+       'НЕ ЗАКУСИВШИМ УДИЛА, И (ИЛИ) ВЫПОЛНЯЮЩИМ ФУНКЦИИ ШТРИБАНА.'
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Тест на штрибана")
+
+
+@bot.message_handler(content_types=['text'])
+def stop_striban(message):
+    print(message.from_user.id)
+    if message.from_user.id == ID:
+        bot.reply_to(message, text)
+
+
+bot.infinity_polling()
